dc5745104e8f30c46817184a1fc6920e301e9693
hatkeson <hatkeson@socal.rr.com>
2021-05-31 15:23:49 -0700

    added ggplot visuals

--- Pool Permit Web Scraping.R
+++ Pool Permit Web Scraping.R
@@ -1,4 +1,9 @@
 library(robotstxt)
+library(lubridate)
+library(magrittr)
+library(tidyverse)
+library(openxlsx)
+library(here)
 
 urls = c("https://mcesearch.monroecounty-fl.gov/search/permits/",
          "https://accela.maricopa.gov/CitizenAccessMCOSS/Cap/CapHome.aspx?module=PnD&TabName=PnD",
@@ -12,3 +17,121 @@ urls = c("https://mcesearch.monroecounty-fl.gov/search/permits/",
          "https://energovcitizenaccess.tylertech.com/WakeCountyNC/SelfService#/search")
 
 paths_allowed(urls)
+
+sum(is.na(pool_permits$Address))
+
+pool_permits_trimmed <- pool_permits %>% 
+  select(Date, `Permit Number`, `Permit Type`, Status, Address, County, State)
+
+sum(is.na(pool_permits_trimmed))
+sum(is.na(pool_permits_trimmed$Date)) # missing dates for monroe, contra costa
+sum(is.na(pool_permits_trimmed$Status))
+
+pool_permits_trimmed %>% 
+  filter(County == 'contra_costa') %>% 
+  nrow
+
+pool_permits <- read_csv("Pool Permits/data/pool_permits.csv")
+
+
+# use R to join dataframes
+
+# monroe
+monroe <- read_csv("Pool Permits/data/monroe.csv")
+wake <- read_csv("Pool Permits/data/wake.csv")
+csv <- read_csv("Pool Permits/data/csv.csv")
+
+trimmed_csv <- csv %>% 
+  select(Date, `Permit Number`, `Permit Type`, Status, Address, County, State)
+
+trimmed_monroe <- monroe %>% 
+  select(Date, `Permit Number`, Type, Status, Address, County, State) %>% 
+  rename(`Permit Type` = Type)
+
+trimmed_wake <- wake %>% 
+  select(Date, `Permit Number`, `Permit Type`, Status, Address, County, State)
+
+full <- rbind(trimmed_csv, trimmed_monroe, trimmed_wake)
+
+full_na <- full[rowSums(is.na(full)) > 0,]
+nrow(full_na) # 528 entries have at least 1 value missing
+
+# by variable and county, how many values are missing?
+full %>% 
+  group_by(County) %>% 
+  summarise(date_na = sum(is.na(Date)),
+            status_na = sum(is.na(Status)),
+            type_na = sum(is.na(`Permit Type`)),
+            number_na = sum(is.na(`Permit Number`)),
+            address_na = sum(is.na(Address))) 
+
+write.xlsx(full, here::here("eight_counties.xlsx"))
+write.csv(full, here::here("eight_counties.csv"))
+
+
+# histogram by year for 2019 - 2020
+
+full %>% 
+  filter(year(Date) >= 2019) %>% 
+  mutate(month = month(Date, label = TRUE),
+         month = as.factor(month),
+         year = as.factor(year(Date))) %>% 
+  ggplot(aes(x = month)) +
+  geom_bar() +
+  facet_grid(cols = vars(year)) +
+  labs(y = "Pool Permit Applications",
+       x = "Month",
+       title = "Pool Permit Applications by Month",
+       caption = "Data gathered on May 30th, 2021")
+
+full %>%
+  mutate(County = as.factor(County),
+         State = as.factor(State)) %>% 
+  group_by(County) %>% 
+  ggplot(aes(y = County)) +
+  geom_bar() + 
+  labs(title = "Pool Permits by County, 1987 - Present",
+       y = "County",
+       x = "Pool Permit Applications", 
+       caption = "Data gathered on May 30th, 2021")
+  
+full %>%
+  filter(year(Date) >= 2019) %>% 
+  mutate(County = as.factor(County),
+         State = as.factor(State)) %>% 
+  group_by(County) %>% 
+  ggplot(aes(y = County)) +
+  geom_bar() + 
+  labs(title = "Pool Permits by County, 2019 - Present",
+       y = "County",
+       x = "Pool Permit Applications",
+       caption = "Data gathered on May 30th, 2021")
+
+# how many entries have a type that contain "Residential" or "Commercial"?
+full %>% 
+  filter(str_detect(string = `Permit Type`, 
+                    pattern = regex('residential', ignore_case = T))) %>% 
+  distinct(County)
+# monroe and san mateo don't tag residential in type
+
+full %>% 
+  filter(str_detect(string = `Permit Type`, 
+                    pattern = regex('commercial', ignore_case = T))) %>% 
+  distinct(County)
+# same, monroe and san mateo don't tag
+
+commercial_residential_proportions <- full %>% 
+  filter(!is.na(Date)) %>% 
+  mutate(after_2019 = case_when(year(Date) > 2019 ~ TRUE,
+                                year(Date) <= 2019 ~ FALSE),
+         comm_or_res = case_when(str_detect(string = `Permit Type`, 
+                                            pattern = regex('commercial', ignore_case = T)) ~ "Commercial",
+                                 str_detect(string = `Permit Type`, 
+                                            pattern = regex('residential', ignore_case = T)) ~ "Residential",
+                                 TRUE ~ "Unknown")) %>% 
+  group_by(after_2019, comm_or_res) %>% 
+  summarize(count = n()) %>% 
+  mutate(proportion = count / sum(count))
+
+write.xlsx(commercial_residential_proportions, 
+           here::here("commercial_residential_proportions.xlsx"))
--- Pool Permit Web Scraping.py
+++ Pool Permit Web Scraping.py
@@ -1,4 +1,5 @@
 #import csv
+from typing import Text
 import numpy as np
 from datetime import timedelta
 import os
@@ -21,8 +22,11 @@ from watchdog.observers import Observer
 from watchdog.events import LoggingEventHandler
 from watchdog.events import FileSystemEventHandler
 
+total_tic = time.perf_counter()
+
 PATH = "C:\\Program Files (x86)\\chromedriver.exe"
 driver = webdriver.Chrome(PATH)
+driver.implicitly_wait(10)
 
 download_dir = ''
 with OpenKey(HKEY_CURRENT_USER, 'SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\Shell Folders') as key:
@@ -152,20 +156,20 @@ def scrape_csv():
         df['State'] = states[i]
 
         if 'Record Number' in df.columns:
-            df.rename({'Record Number' : 'Permit Number', 'Record Type' : 'Permit Type'}, 
+            df.rename({'Record Number' : 'Permit Number', 
+                       'Record Type' : 'Permit Type',
+                       'File Date' : 'Date',
+                       }, 
                       axis = "columns",
                       inplace = True)
         if 'Application Date' in df.columns:
             df.rename({'Application Date' : 'Date'}, axis = "columns", inplace = True)
 
-        # TODO: manually filter san_mateo by Permit Type = "Building Permit"
+        # manually filter san_mateo by Permit Type = "Building Permit"
         # and description contains "pool" case-insensitive
-        # Problem: no entries left after filtering
         if (counties[i] == 'san_mateo'):
             df = df[df['Permit Type'] == 'Building Permit']
             df = df[df['Description'].str.contains('pool', regex = False, case = False, na = False)]
-            print("Filtered San Mateo Dataframe:")
-            print(df)
 
         site_frames.append(df)
 
@@ -173,6 +177,9 @@ def scrape_csv():
         print("Scraped " + counties[i] + " county in: ", toc)
 
     multiple_site_df = concat(site_frames)
+
+    multiple_site_df['Date']= pandas.to_datetime(multiple_site_df['Date'], format = '%m/%d/%Y', errors = 'coerce')
+
     return multiple_site_df
 
 def scrape_monroe():
@@ -247,8 +254,8 @@ def scrape_monroe():
         for c in range(1, cols + 1):
             col_list = []
             for r in range(1, rows + 1):
-                value = driver.find_element_by_xpath(
-                    "/html/body/div[2]/div/div[2]/div/div/div/div[2]/div/table/tbody/tr["+str(r)+"]/td["+str(c)+"]").text
+                value = WebDriverWait(driver, 10).until(
+                        EC.presence_of_element_located((By.XPATH, "/html/body/div[2]/div/div[2]/div/div/div/div[2]/div/table/tbody/tr["+str(r)+"]/td["+str(c)+"]"))).text
                 values_list[c - 1].append(value)
 
 
@@ -262,7 +269,10 @@ def scrape_monroe():
     monroe_df = pandas.DataFrame(dict(zip(header_list, values_list)))
 
     # convert dates from string to Datetime
-    monroe_df['Apply Date']= pandas.to_datetime(monroe_df['Apply Date'], format = '%m-%d-%Y', errors = 'coerce')
+    monroe_df.rename({'Apply Date' : 'Date'}, axis = "columns", inplace = True)
+
+    # change to datetime after combining with all others
+    monroe_df['Date']= pandas.to_datetime(monroe_df['Date'], format = '%m-%d-%Y', errors = 'coerce')
     monroe_df['Permit Issue']= pandas.to_datetime(monroe_df['Permit Issue'], format = '%m-%d-%Y', errors = 'coerce')
 
     # end timer
@@ -372,7 +382,7 @@ def scrape_wake():
             EC.element_to_be_clickable((By.ID, "button-Search")))
         search_button.click()
 
-        time.sleep(2)
+        time.sleep(5)
 
         list_size = Select(driver.find_element_by_id("pageSizeList"))
         list_size.select_by_visible_text("100")
@@ -397,10 +407,22 @@ def scrape_wake():
             
         # scrape first page
         for j in range(0, count):
-            permit_number = driver.find_element_by_xpath("//*[@id=\"entityRecord" + str(j) + "\"]/a/tyler-highlight/span").text
-            applied_date = driver.find_element_by_xpath("//*[@id=\"entityRecordDiv" + str(j) + "\"]/div[2]/div[3]/span").text
-            status = driver.find_element_by_xpath("//*[@id=\"entityRecordDiv" + str(j) + "\"]/div[2]/div[8]/tyler-highlight/span").text
-            address = driver.find_element_by_xpath("//*[@id=\"entityRecordDiv" + str(j) + "\"]/div[2]/div[11]/tyler-highlight/span").text
+
+            permit_number = WebDriverWait(driver, 10).until(
+                EC.presence_of_element_located((By.XPATH, "//*[@id=\"entityRecord" + str(j) + "\"]/a/tyler-highlight/span"))).text
+            applied_date = WebDriverWait(driver, 10).until(
+                EC.presence_of_element_located((By.XPATH, "//*[@id=\"entityRecordDiv" + str(j) + "\"]/div[2]/div[3]/span"))).text
+            status = WebDriverWait(driver, 10).until(
+                EC.presence_of_element_located((By.XPATH, "//*[@id=\"entityRecordDiv" + str(j) + "\"]/div[2]/div[8]/tyler-highlight/span"))).text
+            address = WebDriverWait(driver, 10).until(
+                EC.presence_of_element_located((By.XPATH, "//*[@id=\"entityRecordDiv" + str(j) + "\"]/div[2]/div[11]/tyler-highlight/span"))).text
+
+
+
+            # permit_number = driver.find_element_by_xpath("//*[@id=\"entityRecord" + str(j) + "\"]/a/tyler-highlight/span").text
+            # applied_date = driver.find_element_by_xpath("//*[@id=\"entityRecordDiv" + str(j) + "\"]/div[2]/div[3]/span").text
+            # status = driver.find_element_by_xpath("//*[@id=\"entityRecordDiv" + str(j) + "\"]/div[2]/div[8]/tyler-highlight/span").text
+            # address = driver.find_element_by_xpath("//*[@id=\"entityRecordDiv" + str(j) + "\"]/div[2]/div[11]/tyler-highlight/span").text
             
             permit_numbers.append(permit_number)
             applied_dates.append(applied_date)
@@ -457,12 +479,36 @@ def scrape_wake():
         # weird: there seems to be more rows in the table than the website reports
         print(type_df)
     wake_df = pandas.concat(frames)
+
+    wake_df['Date']= pandas.to_datetime(wake_df['Date'], format = '%m/%d/%Y', errors = 'coerce')
+
+
     return wake_df
 
-        
-d = scrape_csv()
 
-print(d)
+# monroe_df = scrape_monroe()
+# monroe_df.to_csv("data\\monroe.csv")
+
+# wake_df = scrape_wake()
+# wake_df.to_csv("data\\wake.csv")
+
+csv_df = scrape_csv()
+csv_df.to_csv("data\\csv.csv")
+
+# frames = [monroe_df, wake_df, csv_df]
+
+driver.quit()
+
+# final_frame = concat(frames)
+# print(final_frame)
+
+# final_frame.to_excel("data\\pool_permits.xlsx")
+# final_frame.to_csv("data\\pool_permits.csv")
+
+total_toc = timedelta(seconds=time.perf_counter() - total_tic)
+
+print("Total time elapsed: " + str(total_toc))
+
 
 ### Maricopa County, Arizona
 # Not possible to search by status, but shows in results. Issued, Reissued, Final
